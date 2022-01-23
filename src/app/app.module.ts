7a922efe7069373acb1d71c1e17a0ab46bae55d3
Vaari Gupta <vaarigupta24@gmail.com>
2022-01-23 20:03:11 +0530

    removed unwanted code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { WarningAlertComponent } from './warning-alert/warningalert';
-import { SuccessAlertComponent } from './success-alert/successalert';
 import { AppComponent } from './app.component';
 import { TictactoeComponent } from './tictactoe/tictactoe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
     TictactoeComponent
   ],
   imports: [
